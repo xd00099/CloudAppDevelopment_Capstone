77e523f4f732c718d684beabdfe4e41ac2a2996c
xd00099 <xd00099@berkeley.edu>
2023-01-01 19:23:34 -0500

    final commit

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,8 +27,9 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ['localhost', 'localhost:8000',  'https://xd00099-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
+CSRF_TRUSTED_ORIGINS = ['https://xd00099-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 # Application definition
 
